31b5e55933bff41e9bdb32ce6b55c0ea7736ebcd
jheddings <jason@heddings.com>
2018-08-03 22:06:38 -0600

    Added build target for creating plugin structure

--- iplug.mk
+++ iplug.mk
@@ -4,9 +4,10 @@
 PLUGIN_NAME ?= iPlug
 
 BASEDIR := .
+SRCDIR := $(BASEDIR)/src
 
-PLUGIN_DIR ?= $(BASEDIR)/$(PLUGIN_NAME).indigoPlugin
 ZIPFILE ?= $(BASEDIR)/$(PLUGIN_NAME).zip
+PLUGIN_DIR ?= $(BASEDIR)/$(PLUGIN_NAME).indigoPlugin
 PLUGIN_SRC ?= $(PLUGIN_DIR)/Contents/Server Plugin
 
 INDIGO_SUPPORT_DIR ?= /Library/Application Support/Perceptive Automation/Indigo 7
@@ -25,27 +26,37 @@ PY := PYTHONPATH="$(PLUGIN_SRC)" $(shell which python)
 
 DELETE := rm -vf
 RMDIR := rm -vRf
+MKDIR := mkdir -vp
 COPY := cp -fv
 SYNC := rsync -avzP $(foreach patt,$(EXCLUDE_LIST),--exclude '$(patt)')
 
 zip_exclude = $(foreach patt,$(EXCLUDE_LIST),--exclude \$(patt))
 
 ################################################################################
-.PHONY: all clean distclean test dist deploy
+.PHONY: all build rebuild clean distclean test dist deploy
 
 ################################################################################
-test: clean
+build:
+	$(MKDIR) "$(PLUGIN_DIR)/Contents"
+	$(COPY) Info.plist "$(PLUGIN_DIR)/Contents"
+	$(MKDIR) "$(PLUGIN_SRC)"
+	$(COPY) src/* "$(PLUGIN_SRC)"
+	$(COPY) iplug/*.py "$(PLUGIN_SRC)"
+
+################################################################################
+test: clean build
 	$(PY) -m unittest discover -v ./test/
 
 ################################################################################
 dist: zipfile
 
 ################################################################################
-zipfile:
+zipfile: build
 	zip -9r "$(ZIPFILE)" "$(PLUGIN_DIR)" $(zip_exclude)
 
 ################################################################################
 clean:
+	$(RMDIR) "$(PLUGIN_DIR)"
 	find . -name '*.pyc' -exec $(DELETE) {} \;
 
 ################################################################################
@@ -54,9 +65,12 @@ distclean: clean
 	find . -name '*.swp' -exec $(DELETE) {} \;
 
 ################################################################################
-deploy:
+deploy: build
 	$(SYNC) "$(PLUGIN_DIR)" "$(DEPLOY_HOST):$(DEPLOY_PATH)"
 
 ################################################################################
-all: clean test dist
+rebuild: clean build
+
+################################################################################
+all: build test
 

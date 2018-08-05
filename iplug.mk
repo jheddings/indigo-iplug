a77cd1614e27fc7619c91262c8741e19d8bbf1e8
jheddings <jason@heddings.com>
2018-08-04 20:58:00 -0600

    Added support for extra plugin content

--- iplug.mk
+++ iplug.mk
@@ -6,24 +6,38 @@ PLUGIN_NAME ?= iPlug
 BASEDIR ?= .
 SRCDIR ?= $(BASEDIR)/src
 
-ZIPFILE ?= $(BASEDIR)/$(PLUGIN_NAME).zip
-PLUGIN_DIR ?= $(BASEDIR)/$(PLUGIN_NAME).indigoPlugin
-PLUGIN_SRC ?= $(PLUGIN_DIR)/Contents/Server Plugin
+# where iplug is being referenced from...
+IPLUG_BASEDIR ?= $(dir $(lastword $(MAKEFILE_LIST)))
 
+ZIPFILE ?= $(BASEDIR)/$(PLUGIN_NAME).zip
+
+# structure of the plugin
+PLUGIN_BASEDIR ?= $(BASEDIR)/$(PLUGIN_NAME).indigoPlugin
+PLUGIN_CONTENT ?= $(PLUGIN_BASEDIR)/Contents
+PLUGIN_SRC ?= $(PLUGIN_CONTENT)/Server Plugin
+
+# these files will be copied into place for the plugin; they should be referenced
+# according to their paths in the final plugin structure - the most common use is
+# to provide an icon for the bundle
+# source content is expected in an 'etc' directory under BASEDIR
+PLUGIN_ETC_FILES += $(PLUGIN_CONTENT)/Info.plist
+
+# path where Indigo keeps application data
 INDIGO_SUPPORT_DIR ?= /Library/Application Support/Perceptive Automation/Indigo 7
 
-# rsync paths require a bit of extra escaping for some reason...
+# where to deploy the plugin
 DEPLOY_HOST ?= localhost
 DEPLOY_PATH ?= $(INDIGO_SUPPORT_DIR)/Plugins
 
+# these should be ignored for most operations
 EXCLUDE_LIST ?= *.pyc *.swp
 
 # commands used in the makefile
 PY := PYTHONPATH="$(PLUGIN_SRC)" $(shell which python)
-DELETE := rm -vf
-RMDIR := rm -vRf
-MKDIR := mkdir -vp
-COPY := cp -afv
+DELETE := rm -f
+RMDIR := rm -Rf
+MKDIR := mkdir -p
+COPY := cp -af
 ZIP := zip -9r
 RSYNC := rsync -avzP $(foreach patt,$(EXCLUDE_LIST),--exclude '$(patt)')
 
@@ -31,16 +45,22 @@ RSYNC := rsync -avzP $(foreach patt,$(EXCLUDE_LIST),--exclude '$(patt)')
 empty :=
 space := $(empty) $(empty)
 
+src_files = $(wildcard $(SRCDIR)/*)
+dest_files = $(patsubst $(SRCDIR)/%,$(PLUGIN_SRC)/%,$(src_files))
+
 ################################################################################
 .PHONY: all build rebuild clean distclean test dist deploy
 
 ################################################################################
-build:
+build: $(PLUGIN_ETC_FILES)
 	$(MKDIR) "$(PLUGIN_SRC)"
-	$(COPY) $(BASEDIR)/icon.png "$(PLUGIN_DIR)"
-	$(COPY) $(BASEDIR)/Info.plist "$(PLUGIN_DIR)/Contents"
 	$(COPY) $(SRCDIR)/* "$(PLUGIN_SRC)"
-	$(COPY) $(BASEDIR)/iplug/iplug.py "$(PLUGIN_SRC)"
+	$(COPY) $(IPLUG_BASEDIR)/iplug.py "$(PLUGIN_SRC)/iplug.py"
+
+################################################################################
+$(PLUGIN_BASEDIR)/% $(PLUGIN_CONTENT)/%: $(BASEDIR)/etc/%
+	$(MKDIR) "$(dir $@)"
+	$(COPY) "$<" "$@"
 
 ################################################################################
 test: build
@@ -52,11 +72,11 @@ dist: zipfile
 ################################################################################
 zipfile: build
 	$(eval exclude_args := $(foreach patt,$(EXCLUDE_LIST),--exclude \$(patt)))
-	$(ZIP) "$(ZIPFILE)" "$(PLUGIN_DIR)" $(exclude_args)
+	$(ZIP) "$(ZIPFILE)" "$(PLUGIN_BASEDIR)" $(exclude_args)
 
 ################################################################################
 clean:
-	$(RMDIR) "$(PLUGIN_DIR)"
+	$(RMDIR) "$(PLUGIN_BASEDIR)"
 	find . -name '*.pyc' -exec $(DELETE) {} \;
 
 ################################################################################
@@ -67,7 +87,7 @@ distclean: clean
 ################################################################################
 deploy: build
 	$(eval dest_path := $(subst $(space),\$(space),$(DEPLOY_PATH)))
-	$(RSYNC) "$(PLUGIN_DIR)" "$(DEPLOY_HOST):$(dest_path)"
+	$(RSYNC) "$(PLUGIN_BASEDIR)" "$(DEPLOY_HOST):$(dest_path)"
 
 ################################################################################
 rebuild: clean build

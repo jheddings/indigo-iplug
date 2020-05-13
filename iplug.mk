553407577813facabab79325c4a09989be80f238
jheddings <jason@heddings.com>
2020-05-12 20:55:03 -0600

    Refactored phony target declaration

--- iplug.mk
+++ iplug.mk
@@ -44,15 +44,13 @@ src_files = $(wildcard $(SRCDIR)/*)
 dest_files = $(patsubst $(SRCDIR)/%,$(PLUGIN_SRC)/%,$(src_files))
 
 ################################################################################
-.PHONY: all build rebuild clean distclean test dist deploy \
-	upgrade_iplug update_iplug test_iplug is_iplug_repo is_not_iplug_repo
-
-################################################################################
 get_commit_id = $(shell git -C $(BASEDIR) ls-files -s $(1) | cut -f2 -d\ )
 get_commit_status = $(strip $(shell git -C $(BASEDIR) status --short $(1)))
 
 ################################################################################
-build: is_not_iplug_repo
+.PHONY: build
+
+build: is_not_iplug_repo test
 	$(MKDIR) "$(PLUGIN_BASEDIR)"
 	$(MKDIR) "$(PLUGIN_CONTENT)"
 	$(COPY) $(BASEDIR)/etc/Info.plist "$(PLUGIN_CONTENT)"
@@ -61,6 +59,8 @@ build: is_not_iplug_repo
 	$(COPY) $(IPLUG_BASEDIR)/iplug.py "$(PLUGIN_SRC)/iplug.py"
 
 ################################################################################
+.PHONY: test
+
 test: is_not_iplug_repo build
 ifdef TESTMOD
 	$(PY) -m unittest -v $(foreach mod,$(subst $(comma),$(space),$(TESTMOD)),test_$(mod))
@@ -69,57 +69,74 @@ else
 endif
 
 ################################################################################
+.PHONY: dist
+
 dist: is_not_iplug_repo zipfile
 
 ################################################################################
+.PHONY: zipfile
+
 zipfile: is_not_iplug_repo build
 	$(eval exclude_args := $(foreach patt,$(EXCLUDE_LIST),--exclude \$(patt)))
 	$(ZIP) "$(ZIPFILE)" "$(PLUGIN_BASEDIR)" $(exclude_args)
 
 ################################################################################
+.PHONY: clean
+
 clean: is_not_iplug_repo
 	$(RMDIR) "$(PLUGIN_BASEDIR)"
 	find . -name '*.pyc' -exec $(DELETE) {} \;
 
 ################################################################################
-distclean: is_not_iplug_repo clean
+.PHONY: clobber
+
+clobber: is_not_iplug_repo clean
 	$(DELETE) "$(ZIPFILE)"
 	find . -name '*.swp' -exec $(DELETE) {} \;
 
 ################################################################################
+.PHONY: deploy
+
 deploy: is_not_iplug_repo build
 	$(eval dest_path := $(subst $(space),\$(space),$(DEPLOY_PATH)))
 	$(RSYNC) "$(PLUGIN_BASEDIR)" "$(DEPLOY_HOST):$(dest_path)"
 
 ################################################################################
+.PHONY: rebuild
+
 rebuild: is_not_iplug_repo clean build
 
 ################################################################################
+.PHONY: all
+
 all: is_not_iplug_repo build test dist
 
 ################################################################################
+.PHONY: is_iplug_repo
+
 is_iplug_repo:
 ifneq ($(PLUGIN_NAME), iPlug)
 	$(error Must be executed in the iPlug repository)
 endif
 
 ################################################################################
+.PHONY: is_not_iplug_repo
+
 is_not_iplug_repo:
 ifeq ($(PLUGIN_NAME), iPlug)
 	$(error Cannot be executed in the iPlug repository)
 endif
 
 ################################################################################
-# XXX can `make test` just do the right thing here?
-test_iplug: is_iplug_repo
-	#TODO
+.PHONY: update_iplug
 
-################################################################################
 update_iplug: is_not_iplug_repo
 	git -C $(IPLUG_BASEDIR) checkout master
 	git -C $(IPLUG_BASEDIR) pull
 
 ################################################################################
+.PHONY: upgrade_iplug
+
 upgrade_iplug: is_not_iplug_repo update_iplug
 	$(eval iplug_stat = $(call get_commit_status, $(IPLUG_BASEDIR)))
 	$(eval iplug_ver = $(call get_commit_id, $(IPLUG_BASEDIR)))
